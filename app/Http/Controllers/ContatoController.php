fee1e309d82f07c9f71decf1eca6d58d89db10b0
Paulo Cardoso Silva <paulo.silva1514@etec.sp.gov.br>
2024-04-23 17:27:57 -0300

    Contato r,v,c,m

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contato;
+use Exception;
+use Illuminate\Http\Request;
+//use resources\views\contato;
+
+class ContatoController extends Controller
+{
+    /**
+     * Rendeniza a página de contato
+     * 
+     * @return View
+    */
+    public function contato()
+    {
+        return view('contato');
+    }
+
+    /**
+     * 
+    */
+    public function enviarFormulario(Request $request)
+    {
+        try {
+            Contato::create([
+                'nome' => $request->nome,
+                'email' => $request->email,
+                'mensagem' => $request->mensagem,
+                'assunto' => $request->assunto,
+                'telefone' => $request->telefone,
+            ]);
+    
+            return back()->withSucess('Formulário enviado');
+
+        } catch (Exception $exception) {
+            return back()->withErrors($exception->getMessage());
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contato', [ContatoController::class, 'contato']);
+Route::post('contato', [ContatoController::class, 'enviarFormulario']);
